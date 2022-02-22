73b230e97196e258a1ea8a545cba6f7414267d86
ilyaconnectnet <ilya@poem.co.nz>
2022-02-23 07:13:45 +1300

    Error management for no username
    Caused a crash overnight - I'm assuming someone tried to use an invalid twitter username. Wrapped the whole thing in a try catch as a bandaid.

--- utilities.js
+++ utilities.js
@@ -8,58 +8,63 @@ const { TWITTER_CONSUMER, TWITTER_BEARER, TWITTER_SECRET } = require('./config.j
 
 const twitterChannelLoop = async function (handle = "def_ilya", channel) {
 
-    await axios.get(`https://api.twitter.com/2/users/by?usernames=${handle}`, {
-        headers: {
-            Authorization: `Bearer ${TWITTER_BEARER}`
-        }
-    })
-        .then(async (res) => {
-
-            //we try to get the user's id. if the user doesn't exist, we don't establish the loop. 
-            try {
-
-                let _id = res['data']['data'][0]['id']
-                console.log(_id)
-
-                let sentTweet = null;
-
-                let loop = setInterval(async () => {
-                    await axios.get(`https://api.twitter.com/2/users/${_id}/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=5`, {
-                        headers: {
-                            Authorization: `Bearer ${TWITTER_BEARER}`
-                        }
-                    })
-                        .then((res) => {
-                            console.log(res)
-                            try {
-                                lastTweet = res['data']['data'][0]
-
-                                if (lastTweet['id'] != sentTweet) {
-
-                                    const embed = new MessageEmbed()
-                                        .setColor('#0099ff')
-                                        .setTitle('@' + handle)
-                                        .setURL('https://twitter.com/' + handle + "/status/" + lastTweet['id'])
-                                        .setDescription(lastTweet['text'])
-                                        .setTimestamp();
-
-                                    client.channels.cache.get(channel).send({ embeds: [embed] })
-                                    sentTweet = lastTweet['id'];
-                                }
-                            }
-                            catch {
-                                console.log("No tweets found.")
-                            }
-                        })
-
-                }, 10000)
-
-                return loop;
-            }
-            catch {
-                console.log("no twitter user found")
+    try {
+        await axios.get(`https://api.twitter.com/2/users/by?usernames=${handle}`, {
+            headers: {
+                Authorization: `Bearer ${TWITTER_BEARER}`
             }
         })
+            .then(async (res) => {
+
+                //we try to get the user's id. if the user doesn't exist, we don't establish the loop. 
+                try {
+
+                    let _id = res['data']['data'][0]['id']
+                    console.log(_id)
+
+                    let sentTweet = null;
+
+                    let loop = setInterval(async () => {
+                        await axios.get(`https://api.twitter.com/2/users/${_id}/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=5`, {
+                            headers: {
+                                Authorization: `Bearer ${TWITTER_BEARER}`
+                            }
+                        })
+                            .then((res) => {
+                                console.log(res)
+                                try {
+                                    lastTweet = res['data']['data'][0]
+
+                                    if (lastTweet['id'] != sentTweet) {
+
+                                        const embed = new MessageEmbed()
+                                            .setColor('#0099ff')
+                                            .setTitle('@' + handle)
+                                            .setURL('https://twitter.com/' + handle + "/status/" + lastTweet['id'])
+                                            .setDescription(lastTweet['text'])
+                                            .setTimestamp();
+
+                                        client.channels.cache.get(channel).send({ embeds: [embed] })
+                                        sentTweet = lastTweet['id'];
+                                    }
+                                }
+                                catch {
+                                    console.log("no tweets found.")
+                                }
+                            })
+
+                    }, 10000)
+
+                    return loop;
+                }
+                catch {
+                    console.log("user lookup went wrong.")
+                }
+            })
+    }
+    catch {
+        console.log("couldn't get that username.")
+    }
 }
 
 
